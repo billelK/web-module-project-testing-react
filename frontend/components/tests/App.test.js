09a130c26ae6276ee9c8a78319347cde2e084bb6
Billel Khansal <bilalkhansal@gmail.com>
2024-05-31 01:33:18 +0200

    Sprint 11 Module 1 Solved

--- frontend/components/tests/App.test.js
+++ frontend/components/tests/App.test.js
@@ -18,26 +18,36 @@ describe('Stranger Things App', () => {
   test('App mounts without crashing', () => {
     // 👉 TASK: print the simulated DOM using screen.debug
   })
-  test('App renders the correct texts', async () => {
+  test('App renders the correct texts', async () => {  
     // 👉 TASK: click on the button that displays "Press to Get Show Data"
-
+    const button = screen.getByRole("button")
+    expect(button).toBeInTheDocument() 
+    user.click(button)
     // 👉 TASK: create a waitFor and await for the following to be true:
     //    - The text "Press to Get Show Data" is no longer in the DOM
     //    - The text "Stranger Things" exists in the DOM
     //    - The text "A love letter to the '80s classics that captivated a generation" exists in the DOM
     //    - The text "Select A Season" exists in the DOM
     // ❗ You will need { exact: false } to select the longer text
-
+    await waitFor (()=> {
+      expect(button).not.toBeInTheDocument()
+      expect(screen.getByText("Stranger Things")).toBeInTheDocument()
+      expect(screen.getByText(/A love letter to the '80s/i )).toBeInTheDocument()
+      expect(screen.getByText("Select A Season")).toBeInTheDocument()
+    }) 
     // 👉 TASK: select Season 2 from the dropdown
     // ❗ Don't forget user actions need the await keyword
     // ❗ Use the selectOptions user action
     // ❗ Grab the select element using querySelector
-
+    const selectEl = screen.getByTestId("seasons")
+    await user.selectOptions(selectEl, "Season 2")
     // 👉 TASK: create the following assertions:
     //    - The text "Season 2, Episode 1" exists in the DOM
     //    - The text "Chapter One: MADMAX" exists in the DOM
     //    - The text "One year after the events with the Upside Down and the Demogorgon" exists in the DOM
     // ❗ You will need { exact: false } to select the longer text
-
+    expect(screen.getByText("Season 2, Episode 1")).toBeInTheDocument()
+    expect(screen.getByText("Chapter One: MADMAX")).toBeInTheDocument()
+    expect(screen.getByText(/one year after the events/i)).toBeInTheDocument()
   })
 })
